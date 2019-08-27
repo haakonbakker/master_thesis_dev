749e81891b8fac9651b059c275b720986686f836
Haakon Bakker <haakonhoelbakker@gmail.com>
2019-08-27 21:36:44 +0200

    Initial Commit

---
+++ osa_tracker/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  osa_tracker
+//
+//  Created by Haakon W Hoel Bakker on 27/08/2019.
+//  Copyright © 2019 Haakon W Hoel Bakker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+// Plan for the day:
+// - Create quick outline for application
+// - Start working on getting mic on phone to work
+// - Make the git repo for this project
